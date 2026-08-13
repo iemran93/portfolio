bc5fa8d6a29ad180af2809475186a8749ab9f7df
emran <emran-93@outlook.com>
2026-08-13 23:26:23 +0300

    fix: ts types

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,7 @@ import VueRouter from 'vue-router/vite'
 import Markdown from 'unplugin-vue-markdown/vite'
 import MarkdownItAnchor from 'markdown-it-anchor'
 import MarkdownItPrism from 'markdown-it-prism'
+import MarkdownIt from 'markdown-it'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -27,16 +28,18 @@ export default defineConfig({
         typographer: true,
       },
       // A function providing the markdown-exit instance gets the ability to apply custom settings/plugins
-      markdownSetup(md) {
-        // for example
-        md.use(MarkdownItAnchor, {
+      markdownItSetup(md) {
+        const mdInstance = md as unknown as MarkdownIt
+
+        mdInstance.use(MarkdownItAnchor, {
           permalink: MarkdownItAnchor.permalink.linkInsideHeader({
             symbol:
               '<svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"><path d="M10 13a5 5 0 0 0 7.54.54l3-3a5 5 0 0 0-7.07-7.07l-1.72 1.71"/><path d="M14 11a5 5 0 0 0-7.54-.54l-3 3a5 5 0 0 0 7.07 7.07l1.71-1.71"/></svg>',
             placement: 'before',
           }),
         })
-        md.use(MarkdownItPrism)
+
+        mdInstance.use(MarkdownItPrism)
       },
       // Class names for the wrapper div
       wrapperClasses: 'markdown-body',
